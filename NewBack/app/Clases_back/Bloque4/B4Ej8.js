5c7fb79cd44def729903875e4957dfb1120d042a
dejaimenpax <jaimegonzalezcasero@gmail.com>
2023-10-09 22:54:49 +0200

    Añade cambios ejercicios Bloque 4: erratas y feedback mejorado

--- NewBack/app/Clases_back/Bloque4/B4Ej8.js
+++ NewBack/app/Clases_back/Bloque4/B4Ej8.js
@@ -3,7 +3,7 @@ const EjGenerico = require('../EjGenerico.js');
 class B4Ej8 extends EjGenerico {
 
   constructor(
-    texto = "Halla el máximo, la media aritmética y el mínimo del siguiente conjunto de números, redondeando las tres respuestas al entero más cercano.",
+    texto = "Halla el máximo, la media aritmética y el mínimo del siguiente conjunto de números, redondeando la media al entero más cercano.",
     enunciado = [
       `${Array.from({length: 15}, () => Math.floor(Math.random() * (50 - 2)) + 2).join(' ')}`
     ],
@@ -21,7 +21,7 @@ class B4Ej8 extends EjGenerico {
 
     this.long_input = 3;
     this.etiquetas= ["Máximo:", "Media aritmética (redondeada a la unidad):", "Mínimo:"]
-    this.puntos_explicados = "Las respuestas correctas para el máximo y el mñinimo valen 2.5 puntos cada una. Acertar la media cuenta 5 puntos."
+    this.puntos_explicados = "Las respuestas correctas para el máximo y el mínimo valen 2.5 puntos cada una. Acertar la media cuenta 5 puntos."
 
   }
 
@@ -31,7 +31,8 @@ class B4Ej8 extends EjGenerico {
   
     const numeros = this.enunciado[0].split(' ').map(numero => parseInt(numero));
     const maximo = Math.max(...numeros);
-    const media = numeros.reduce((suma, numero) => suma + numero, 0) / numeros.length;
+    const sumaTotal = numeros.reduce((suma, numero) => suma + numero, 0);
+    const media = sumaTotal / numeros.length;
     const minimo = Math.min(...numeros);
   
     const respuestaMaximo = parseInt(this.input[0]);
@@ -39,21 +40,21 @@ class B4Ej8 extends EjGenerico {
     const respuestaMinimo = parseInt(this.input[2]);
   
     if (respuestaMaximo === maximo) {
-      this.explicacion.push(`¡Es correcto!`);
+      this.explicacion.push(`¡Es correcto! El máximo se corresponde con el número más grande de la lista.`);
       this.nota += this.puntuacion/4;
     } else {
       this.explicacion.push(`No es correcto. El máximo es ${maximo}. Para calcularlo se debe buscar el número más grande de la lista.`);
     }
   
     if (respuestaMedia === Math.round(media)) {
-      this.explicacion.push(`¡Es correcto!`);
+      this.explicacion.push(`¡Es correcto! Para hallarla, se han sumado todos los números (obteniendo ${sumaTotal}) y después dividido por la cantidad de números (${numeros.length}).`);
       this.nota += this.puntuacion/2; //vale mas calcular la media que el resto
     } else {
-      this.explicacion.push(`No es correcto. La media es ${Math.round(media)}. Para calcularla se debe sumar todos los números y dividir por la cantidad de números.`);
+      this.explicacion.push(`No es correcto. La media es ${Math.round(media)}. Para calcularla se debe sumar todos los números (obteniendo ${sumaTotal}) y dividir por la cantidad de números (${numeros.length}).`);
     }
   
     if (respuestaMinimo === minimo) {
-      this.explicacion.push(`¡Es correcto!`);
+      this.explicacion.push(`¡Es correcto! El mínimo se corresponde con el número más pequeño de la lista.`);
       this.nota += this.puntuacion/4;
     } else {
       this.explicacion.push(`No es correcto. El mínimo es ${minimo}. Para calcularlo se debe buscar el número más pequeño de la lista.`);
